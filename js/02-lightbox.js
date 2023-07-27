0f129406b27693d72264ab2390b2a5171a2d6c33
Alona <polejcukalona@gmail.com>
2023-07-28 00:01:46 +0300

    dfhlrhgjh

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+document.addEventListener('DOMContentLoaded', function () {
+  const gallery = document.querySelector('.gallery');
+
+  function createGalleryItem(item) {
+    const galleryItem = document.createElement('li');
+    galleryItem.classList.add('gallery__item');
+
+    const link = document.createElement('a');
+    link.classList.add('gallery__link');
+    link.href = item.original;
+
+    const image = document.createElement('img');
+    image.classList.add('gallery__image');
+    image.src = item.preview;
+    image.alt = item.description;
+    image.setAttribute('data-source', item.original);
+
+    link.appendChild(image);
+    galleryItem.appendChild(link);
+
+    return galleryItem;
+  }
+
+  galleryItems.forEach(item => {
+    const galleryItem = createGalleryItem(item);
+    gallery.appendChild(galleryItem);
+  });
+
+      var lightbox = new SimpleLightbox('.gallery a', 
+      {
+        captionsData: 'alt',
+        captions: true, 
+        fadeSpeed: 250,
+    });
+});
